2c399baf66026c24c896070b769ca3b52b3b5477
Brandon Rankin <brankin111@gmail.com>
2020-11-28 19:19:08 -0500

    added GSAP animations to each product section

--- main.js
+++ main.js
@@ -4,8 +4,56 @@ $('.banner')
     .hide()
     .slideDown(2500);
 
+
 // CAROUSEL //
 
 $('.carousel').carousel({
     interval: 6000
 });
+
+
+// GSAP ANIMATIONS //
+
+gsap.from('.new-products-grid', {
+    scrollTrigger: {
+        trigger: '.new-products-grid',
+        toggleActions: 'play reverse play reverse',
+        start: 'top top',
+    },
+    duration: 1.5,
+    x: '150%',
+    delay: .01,
+})
+
+gsap.from('.laptops-grid', {
+    scrollTrigger: {
+        trigger: '.laptops-grid',
+        toggleActions: 'play reverse play reverse',
+        start: 'top top',
+    },
+    duration: 1.5,
+    x: '-150%',
+    delay: .01,
+})
+
+gsap.from('.desktops-grid', {
+    scrollTrigger: {
+        trigger: '.desktops-grid',
+        toggleActions: 'play reverse play reverse',
+        start: 'top top',
+    },
+    duration: 1.5,
+    x: '150%',
+    delay: .01,
+})
+
+gsap.from('.carousel', {
+    scrollTrigger: {
+        trigger: '.carousel',
+        toggleActions: 'play reverse play reverse',
+        start: 'top top',
+    },
+    duration: 1.5,
+    x: '-200%',
+    delay: .01,
+})
