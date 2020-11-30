baa74e501f8f25e6dca47afffd8fa16c86e72a56
Brandon Rankin <brankin111@gmail.com>
2020-11-29 23:35:20 -0500

    updated GSAP animations in main.js and changed all product image hovers in styles.css.

--- main.js
+++ main.js
@@ -1,59 +1,70 @@
-// HEADER ANIMATION //
+// HEADER IMAGE ANIMATION //
 
-$('.banner')
-    .hide()
-    .slideDown(2500);
+//$('.banner')
+//    .hide()
+//    .slideDown(2500);
 
+// GSAP ANIMATIONS //
+
+gsap.from('#banner-img', {
+    duration: 2.5,
+    scale: 3,
+    ease: 'expoScale(1, 3)'
+});
+
+gsap.from('.new-products-grid', {
+    scrollTrigger: {
+        trigger: '.new-products-grid',
+        toggleActions: 'play reverse play reverse',
+        start: '-105% top',
+        end: '+=135%',
+        //markers: true,
+    },
+    duration: 1.5,
+    x: '150%',
+    delay: .01,
+});
+
+gsap.from('.laptops-grid', {
+    scrollTrigger: {
+        trigger: '.laptops-grid',
+        toggleActions: 'play reverse play reverse',
+        start: '-90% top',
+        end: '+=135%',
+        //markers: true,
+    },
+    duration: 1.5,
+    x: '-150%',
+    delay: .01,
+});
+
+gsap.from('.desktops-grid', {
+    scrollTrigger: {
+        trigger: '.desktops-grid',
+        toggleActions: 'play reverse play reverse',
+        start: '-90% top',
+        end: '+=135%',
+        //markers: true,
+    },
+    duration: 1.5,
+    x: '150%',
+    delay: .01,
+});
+
+gsap.from('.carousel', {
+    scrollTrigger: {
+        trigger: '.carousel',
+        toggleActions: 'play reverse play reverse',
+        start: '-115% top',
+        //markers: true,
+    },
+    duration: 1.5,
+    x: '-200%',
+    delay: .01,
+});
 
 // CAROUSEL //
 
 $('.carousel').carousel({
     interval: 6000
 });
-
-
-// GSAP ANIMATIONS //
-
-gsap.from('.new-products-grid', {
-    scrollTrigger: {
-        trigger: '.new-products-grid',
-        toggleActions: 'play reverse play reverse',
-        start: 'top top',
-    },
-    duration: 1.5,
-    x: '150%',
-    delay: .01,
-})
-
-gsap.from('.laptops-grid', {
-    scrollTrigger: {
-        trigger: '.laptops-grid',
-        toggleActions: 'play reverse play reverse',
-        start: 'top top',
-    },
-    duration: 1.5,
-    x: '-150%',
-    delay: .01,
-})
-
-gsap.from('.desktops-grid', {
-    scrollTrigger: {
-        trigger: '.desktops-grid',
-        toggleActions: 'play reverse play reverse',
-        start: 'top top',
-    },
-    duration: 1.5,
-    x: '150%',
-    delay: .01,
-})
-
-gsap.from('.carousel', {
-    scrollTrigger: {
-        trigger: '.carousel',
-        toggleActions: 'play reverse play reverse',
-        start: 'top top',
-    },
-    duration: 1.5,
-    x: '-200%',
-    delay: .01,
-})
