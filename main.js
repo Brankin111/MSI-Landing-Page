a13b80e100a4ff0b511e5275aec93e3a9f182f44
Brandon Rankin <brankin111@gmail.com>
2020-11-30 16:27:44 -0500

    added a .matchMeadia function in main.js to kill all GSAP animations on screen sizes below 1200px

--- main.js
+++ main.js
@@ -6,62 +6,70 @@
 
 // GSAP ANIMATIONS //
 
-gsap.from('#banner-img', {
-    duration: 3,
-    scale: 3,
-    ease: 'expoScale(1, 3)',
-    delay: .75
-});
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.new-products-grid', {
-    scrollTrigger: {
-        trigger: '.new-products-grid',
-        toggleActions: 'play reverse play reverse',
-        start: '-105% top',
-        end: '+=160%',
-        //markers: true,
-    },
-    duration: 1.5,
-    x: '150%',
-    delay: .01,
-});
+ScrollTrigger.matchMedia({
 
-gsap.from('.laptops-grid', {
-    scrollTrigger: {
-        trigger: '.laptops-grid',
-        toggleActions: 'play reverse play reverse',
-        start: '-100% top',
-        end: '+=160%',
-        //markers: true,
-    },
-    duration: 1.5,
-    x: '-150%',
-    delay: .01,
-});
+    "(min-width: 1200px)": function () {
 
-gsap.from('.desktops-grid', {
-    scrollTrigger: {
-        trigger: '.desktops-grid',
-        toggleActions: 'play reverse play reverse',
-        start: '-100% top',
-        end: '+=160%',
-        //markers: true,
-    },
-    duration: 1.5,
-    x: '150%',
-    delay: .01,
-});
+        //        gsap.from('#banner-img', {
+        //            duration: 3,
+        //            scale: 3,
+        //            ease: 'expoScale(1, 3)',
+        //            delay: .75
+        //        });
 
-gsap.from('.carousel', {
-    scrollTrigger: {
-        trigger: '.carousel',
-        toggleActions: 'play reverse play reverse',
-        start: '-145% top',
-        //markers: true,
-    },
-    duration: 1.5,
-    x: '-200%',
-    delay: .01,
+        gsap.from('.new-products-grid', {
+            scrollTrigger: {
+                trigger: '.new-products-grid',
+                toggleActions: 'play reverse play reverse',
+                start: '-105% top',
+                end: '+=160%',
+                //markers: true,
+            },
+            duration: 1.5,
+            x: '150%',
+            delay: .01,
+        });
+
+        gsap.from('.laptops-grid', {
+            scrollTrigger: {
+                trigger: '.laptops-grid',
+                toggleActions: 'play reverse play reverse',
+                start: '-100% top',
+                end: '+=160%',
+                //markers: true,
+            },
+            duration: 1.5,
+            x: '-150%',
+            delay: .01,
+        });
+
+        gsap.from('.desktops-grid', {
+            scrollTrigger: {
+                trigger: '.desktops-grid',
+                toggleActions: 'play reverse play reverse',
+                start: '-100% top',
+                end: '+=160%',
+                //markers: true,
+            },
+            duration: 1.5,
+            x: '150%',
+            delay: .01,
+        });
+
+        gsap.from('.carousel', {
+            scrollTrigger: {
+                trigger: '.carousel',
+                toggleActions: 'play reverse play reverse',
+                start: '-145% top',
+                //markers: true,
+            },
+            duration: 1.5,
+            x: '-200%',
+            delay: .01,
+        });
+    }
 });
 
 // CAROUSEL //
