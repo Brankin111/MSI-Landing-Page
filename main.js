14ef590cf6c0cd635b4c888b33a563d2844993ab
Brandon Rankin <brankin111@gmail.com>
2020-12-07 18:31:30 -0500

    changed banner-img to fade in on page load. added a scrub value of 4 to laptops-grid and desktop-grid, attempting to fix the animation start/ stop issue.

--- main.js
+++ main.js
@@ -8,89 +8,90 @@ $('.carousel').carousel({
 //BANNER ANIMATION //
 
 gsap.from('#banner-img', {
-    duration: 3,
-    scale: 3,
-    ease: 'expoScale(1, 3)',
-    delay: .75
+    duration: 4,
+    opacity: 0,
+    delay: 1
 });
 
 // PRODUCT SECTION SCROLLTRIGGER ANIMATIONS //
 //***** BROKEN :( *****//
 
-//gsap.registerPlugin(ScrollTrigger);
-//
-//ScrollTrigger.matchMedia({
-//    "(min-width: 1300px)": function () {
-//
-//        // MONITOR SECTION ANIMATIONS //
-//        const monitorsTimeline = gsap.timeline({
-//            scrollTrigger: {
-//                trigger: '.new-products-grid',
-//                toggleActions: 'play reverse play reverse',
-//                markers: true
-//            },
-//            defaults: {
-//                duration: 1.5,
-//                start: 'top bottom',
-//                end: 'bottom top'
-//            }
-//        });
-//
-//        monitorsTimeline.from('.new-products-grid', {
-//            x: '150%',
-//        });
-//
-//        // LAPTOP SECTION ANIMATIONS //
-//        const laptopsTimeline = gsap.timeline({
-//            scrollTrigger: {
-//                trigger: '.laptops-grid',
-//                toggleActions: 'play reverse play reverse',
-//                markers: true
-//            },
-//            defaults: {
-//                duration: 1.5,
-//                start: 'top bottom',
-//                end: 'bottom top',
-//            }
-//        });
-//
-//        laptopsTimeline.from('.laptops-grid', {
-//            x: '-150%',
-//        });
-//
-//        // DESKTOP SECTION ANIMATIONS //
-//        const desktopsTimeline = gsap.timeline({
-//            scrollTrigger: {
-//                trigger: '.desktops-grid',
-//                toggleActions: 'play reverse play reverse',
-//                markers: true
-//            },
-//            defaults: {
-//                duration: 1.5,
-//                start: 'top bottom',
-//                end: 'bottom top',
-//            }
-//        });
-//
-//        desktopsTimeline.from('.desktops-grid', {
-//            x: '150%',
-//        });
-//
-//        // MASCOT SECTION ANIMATIONS //
-//        const carouselTimeline = gsap.timeline({
-//            scrollTrigger: {
-//                trigger: '.carousel',
-//                toggleActions: 'play reverse play reverse',
-//                markers: true
-//            },
-//            defaults: {
-//                duration: 1.5,
-//                start: 'top bottom',
-//            }
-//        });
-//
-//        carouselTimeline.from('.carousel', {
-//            x: '-200%',
-//        });
-//    }
-//});
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.matchMedia({
+    "(min-width: 1300px)": function () {
+
+        // MONITOR SECTION ANIMATIONS //
+        let monitorsTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.new-products-grid',
+                toggleActions: 'play reverse play reverse',
+                markers: true,
+                start: 'top bottom',
+                end: 'bottom top'
+            },
+            defaults: {
+                duration: 1.5
+            }
+        });
+
+        monitorsTimeline.from('.new-products-grid', {
+            x: innerWidth * 1
+        });
+
+        // LAPTOP SECTION ANIMATIONS //
+        let laptopsTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.laptops-grid',
+                toggleActions: 'play reverse play reverse',
+                markers: true,
+                start: 'top bottom',
+                end: 'bottom bottom',
+                scrub: 4
+            },
+            defaults: {
+                //duration: 1.5
+            }
+        });
+
+        laptopsTimeline.from('.laptops-grid', {
+            x: innerWidth * -1
+        });
+
+        // DESKTOP SECTION ANIMATIONS //
+        let desktopsTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.desktops-grid',
+                toggleActions: 'play reverse play reverse',
+                markers: true,
+                start: 'top bottom',
+                end: 'bottom bottom',
+                scrub: 4
+            },
+            defaults: {
+                //duration: 1.5
+            }
+        });
+
+        desktopsTimeline.from('.desktops-grid', {
+            x: innerWidth * 1
+        });
+
+        // MASCOT SECTION ANIMATIONS //
+        //        const carouselTimeline = gsap.timeline({
+        //            scrollTrigger: {
+        //                trigger: '.carousel',
+        //                toggleActions: 'play reverse play reverse',
+        //                markers: true
+        //            },
+        //            defaults: {
+        //                duration: 1.5,
+        //                start: 'top bottom',
+        //            }
+        //        });
+        //
+        //        carouselTimeline.from('.carousel', {
+        //            x: '-200%',
+        //        });
+    }
+});
