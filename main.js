f52a2c00fa5e4fbcb611726dcfd74863367981ac
Jason Graham <jasongraham@cloudfitsoftware.com>
2020-12-20 21:36:17 -0500

    used jQuery document.ready in main.js

--- main.js
+++ main.js
@@ -1,79 +1,77 @@
-// CAROUSEL //
+$(document).ready(function () {
 
-$('.carousel').carousel({
-    interval: 6000
-});
-
-
-//BANNER ANIMATION //
-
-gsap.from('#banner-img', {
-    duration: 1,
-    opacity: 0,
-    delay: .1
-});
+    // CAROUSEL //
+    $('.carousel').carousel({
+        interval: 6000
+    });
 
 
-// PRODUCT SECTION SCROLLTRIGGER ANIMATIONS //
-
-gsap.registerPlugin(ScrollTrigger);
-
-ScrollTrigger.matchMedia({
-    "(min-width: 1300px)": function () {
-
-
-        // MONITOR SECTION ANIMATIONS //
-        var monitorsTimeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.new-products-grid',
-                toggleActions: 'play reverse play reset',
-                start: 'top bottom',
-                end: 'top center',
-                scrub: 5
-            }
-        });
-
-        monitorsTimeline.from('.new-products-grid', {
-            x: innerWidth * 1
-        });
+    //BANNER ANIMATION //
+    gsap.from('#banner-img', {
+        duration: 1,
+        opacity: 0,
+        delay: .1
+    });
 
 
-        // LAPTOP SECTION ANIMATIONS //
-        var laptopsTimeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.laptops-grid',
-                toggleActions: 'play reverse play reset',
-                start: 'top bottom',
-                end: 'top center',
-                scrub: 5
-            }
-        });
+    // PRODUCT SECTION SCROLLTRIGGER ANIMATIONS //
+    gsap.registerPlugin(ScrollTrigger);
 
-        laptopsTimeline.from('.laptops-grid', {
-            x: innerWidth * -1
-        });
+    ScrollTrigger.matchMedia({
+        "(min-width: 1300px)": function () {
 
 
-        // DESKTOP SECTION ANIMATIONS //
-        var desktopsTimeline = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.desktops-grid',
-                toggleActions: 'play reverse play reverse',
-                start: 'top bottom',
-                end: 'top center',
-                scrub: 5
-            }
-        });
+            // MONITOR SECTION ANIMATIONS //
+            var monitorsTimeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.new-products-grid',
+                    toggleActions: 'play reverse play reset',
+                    start: 'top bottom',
+                    end: 'top center',
+                    scrub: 5
+                }
+            });
 
-        desktopsTimeline.from('.desktops-grid', {
-            x: innerWidth * 1
-        });
-    }
-});
+            monitorsTimeline.from('.new-products-grid', {
+                x: innerWidth * 1
+            });
 
-// PAGE REFRESH IMAGE RELOAD
-$(document).ready(function(){
-    $(window).resize(function(){
+
+            // LAPTOP SECTION ANIMATIONS //
+            var laptopsTimeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.laptops-grid',
+                    toggleActions: 'play reverse play reset',
+                    start: 'top bottom',
+                    end: 'top center',
+                    scrub: 5
+                }
+            });
+
+            laptopsTimeline.from('.laptops-grid', {
+                x: innerWidth * -1
+            });
+
+
+            // DESKTOP SECTION ANIMATIONS //
+            var desktopsTimeline = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.desktops-grid',
+                    toggleActions: 'play reverse play reverse',
+                    start: 'top bottom',
+                    end: 'top center',
+                    scrub: 5
+                }
+            });
+
+            desktopsTimeline.from('.desktops-grid', {
+                x: innerWidth * 1
+            });
+        }
+    });
+
+    // PAGE REFRESH IMAGE RELOAD
+    $(window).resize(function () {
         window.location.href = window.location.href;
-    });  
+    });
 });
