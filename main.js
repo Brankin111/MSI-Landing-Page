af72cf4bea2e30e1ec8b60f6a68c449b62413333
Brandon Rankin <brankin111@gmail.com>
2020-12-21 13:40:26 -0500

    changed scrollTrigger.matchMedia min-width from 1300px to 1500px in main.js

--- main.js
+++ main.js
@@ -1,76 +1,75 @@
+// CAROUSEL //
+$('.carousel').carousel({
+    interval: 6000
+});
+
+
+//BANNER ANIMATION //
+//    gsap.from('#banner-img', {
+//        duration: 1,
+//        opacity: 0,
+//        delay: .1
+//    });
+
+
+// PRODUCT SECTION SCROLLTRIGGER ANIMATIONS //
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.matchMedia({
+    "(min-width: 1500px)": function () {
+
+
+        // MONITOR SECTION ANIMATIONS //
+        var monitorsTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.new-products-grid',
+                toggleActions: 'play reverse play reset',
+                start: 'top bottom',
+                end: 'top center',
+                scrub: 5
+            }
+        });
+
+        monitorsTimeline.from('.new-products-grid', {
+            x: innerWidth * 1
+        });
+
+
+        // LAPTOP SECTION ANIMATIONS //
+        var laptopsTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.laptops-grid',
+                toggleActions: 'play reverse play reset',
+                start: 'top bottom',
+                end: 'top center',
+                scrub: 5
+            }
+        });
+
+        laptopsTimeline.from('.laptops-grid', {
+            x: innerWidth * -1
+        });
+
+
+        // DESKTOP SECTION ANIMATIONS //
+        var desktopsTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.desktops-grid',
+                toggleActions: 'play reverse play reverse',
+                start: 'top bottom',
+                end: 'top center',
+                scrub: 5
+            }
+        });
+
+        desktopsTimeline.from('.desktops-grid', {
+            x: innerWidth * 1
+        });
+    }
+});
+
+// PAGE REFRESH IMAGE RELOAD //
 $(document).ready(function () {
-
-    // CAROUSEL //
-    $('.carousel').carousel({
-        interval: 6000
-    });
-
-
-    //BANNER ANIMATION //
-    //    gsap.from('#banner-img', {
-    //        duration: 1,
-    //        opacity: 0,
-    //        delay: .1
-    //    });
-
-
-    // PRODUCT SECTION SCROLLTRIGGER ANIMATIONS //
-    gsap.registerPlugin(ScrollTrigger);
-
-    ScrollTrigger.matchMedia({
-        "(min-width: 1500px)": function () {
-
-
-            // MONITOR SECTION ANIMATIONS //
-            var monitorsTimeline = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.new-products-grid',
-                    toggleActions: 'play reverse play reset',
-                    start: 'top bottom',
-                    end: 'top center',
-                    scrub: 5
-                }
-            });
-
-            monitorsTimeline.from('.new-products-grid', {
-                x: innerWidth * 1
-            });
-
-
-            // LAPTOP SECTION ANIMATIONS //
-            var laptopsTimeline = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.laptops-grid',
-                    toggleActions: 'play reverse play reset',
-                    start: 'top bottom',
-                    end: 'top center',
-                    scrub: 5
-                }
-            });
-
-            laptopsTimeline.from('.laptops-grid', {
-                x: innerWidth * -1
-            });
-
-
-            // DESKTOP SECTION ANIMATIONS //
-            var desktopsTimeline = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.desktops-grid',
-                    toggleActions: 'play reverse play reverse',
-                    start: 'top bottom',
-                    end: 'top center',
-                    scrub: 5
-                }
-            });
-
-            desktopsTimeline.from('.desktops-grid', {
-                x: innerWidth * 1
-            });
-        }
-    });
-
-    // PAGE REFRESH IMAGE RELOAD
     $(window).resize(function () {
         window.location.href = window.location.href;
     });
